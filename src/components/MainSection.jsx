29af3e61c44d9e6e70e12f849bf23a1a5a7ee48e
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-29 15:52:18 +0100

    default cards

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Col, Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import DefaultImg from "../images/default.png";
 
 const MainSection = () => {
     const [NomeSaga, setNomeSaga] = useState("harry Potter");
     const [arrayDatas, setArrayDatas] = useState(null);
+    const [BeforeInit, SetBeforeInit] = useState(true);
+    const [defaultValues, setDefaultValues] = useState(
+        Array.from({ length: 10 }, () => ({
+            img: DefaultImg,
+            title: "Card Title",
+            parag: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            textbutt1: "view",
+            textbutt2: "Edit",
+            time: "9 min",
+        }))
+    );
     console.log(arrayDatas); /* array con dati della get  */
-
-    useEffect(() => {
-        fetchAGet();
-    }, []); /* lo fa una volta sola così ?? */
 
     const fetchAGet = () => {
         const options = {
@@ -46,21 +54,45 @@ const MainSection = () => {
         <div className="bg-sfondoMain">
             <Container>
                 <div className="d-flex flex-wrap">
-                    {arrayDatas.map((movie, index) => (
-                        <Col sm={12} md={6} lg={4}>
-                            <Card className="m-2 mt-5" key={`key${index}`}>
-                                <Card.Img variant="top" src={movie.Poster} className="dimension-card-img" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
+                    {arrayDatas &&
+                        arrayDatas.map((movie, index) => (
+                            <Col sm={12} md={6} lg={4}>
+                                <Card className="m-2 mt-5" key={`key-fetch-${index}`}>
+                                    <Card.Img variant="top" src={movie.Poster} className="dimension-card-img" />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+
+                    {!arrayDatas &&
+                        defaultValues.map((movie, index) => (
+                            <Col sm={12} md={6} lg={4}>
+                                <Card className="m-2 mt-5" key={`key-default-${index}`}>
+                                    <Card.Img variant="top" src={movie.img} className="dimension-card-img" />
+                                    <Card.Body>
+                                        <Card.Title>{movie.title}</Card.Title>
+                                        <Card.Text>{movie.parag}</Card.Text>
+                                        <div className="d-flex justify-content-between">
+                                            <div>
+                                                {" "}
+                                                <Button variant="btn btn-outline-secondary">{movie.textbutt1}</Button>
+                                                <Button className="ms-2" variant="btn btn-outline-secondary">
+                                                    {movie.textbutt2}
+                                                </Button>
+                                            </div>
+                                            <p>{movie.time}</p>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
                 </div>
             </Container>
         </div>
