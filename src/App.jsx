91340011ac7f529a0f65ef1c0a004c6f825a6374
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-29 12:58:15 +0100

    jumbo jumbo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Jumbo from "./components/Jumbo";
 
 function App() {
     return (
         <div className="App">
             <NavBar />
+            <Jumbo />
         </div>
     );
 }
---
+++ src/components/Jumbo.jsx
@@ -0,0 +1,33 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+
+const Jumbo = () => {
+    return (
+        <div className="bg-light">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="d-flex justify-content-center my-5 p-5">
+                            <div className="text-center">
+                                <h3 className="my-3 fs-1 fw-normal">Lorem, ipsum. dolor</h3>
+                                <p className="mb-4">
+                                    This page will dynamically load pictures from the web, and display them in a cool
+                                    way!
+                                </p>
+                                <Button variant="primary">Load Image</Button>
+                                <Button className="ms-2" variant="secondary">
+                                    Load Second Image
+                                </Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Jumbo;
