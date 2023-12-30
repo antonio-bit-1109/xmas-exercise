c5355f971b9283563cafae7c42d89e9342873aa7
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-30 16:12:10 +0100

    singleCard component

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Container, Row } from "react-bootstrap";
 import DefaultImg from "../images/default.png";
 import InputSearch from "./InputSearch";
+import SingleCard from "./SingleCard";
 
 const MainSection = (props) => {
     const [searchItem, setSearchItem] = useState("summer");
@@ -153,87 +152,14 @@ const MainSection = (props) => {
             </Container>
 
             <Container>
-                <div className="d-flex flex-wrap">
-                    {arrayDatas &&
-                        arrayDatas.slice(0, 9).map((image, index) => (
-                            <Col
-                                style={{ display: cardHidden.includes(image.id) ? "none" : "block" }}
-                                key={`key-fetch-${index}`}
-                                sm={12}
-                                md={6}
-                                lg={4}
-                            >
-                                <Card className="m-2 mt-5 shadow">
-                                    <Card.Img variant="top" src={image.src.original} className="dimension-card-img" />
-                                    <Card.Body style={{ minHeight: "320px" }}>
-                                        <Card.Title>{image.alt}</Card.Title>
-                                        <Card.Text>
-                                            Some quick example text to build on the card title and make up the bulk of
-                                            the card's content.
-                                        </Card.Text>
-                                        <Card.Text className="fw-bold">{image.photographer}</Card.Text>
-                                        <div className="d-flex flex-column">
-                                            {" "}
-                                            <a className="m-1" href={image.src.original}>
-                                                Guarda l'originale
-                                            </a>
-                                            <a className="m-1" href={image.photographer_url}>
-                                                Vai al profilo dell'autore
-                                            </a>
-                                        </div>
-
-                                        <div className="d-flex justify-content-between">
-                                            <div className="d-flex gap-2 mt-2">
-                                                {" "}
-                                                <Button variant="btn btn-outline-secondary">View</Button>
-                                                <Button
-                                                    onClick={() => {
-                                                        handleCardHidden(image.id);
-                                                    }}
-                                                    variant="btn btn-outline-secondary"
-                                                >
-                                                    Hide
-                                                </Button>
-                                                <Button
-                                                    onClick={() => downloadImage(image.src.original, image.alt)}
-                                                    variant="btn btn-outline-primary"
-                                                >
-                                                    Download
-                                                </Button>
-                                            </div>
-                                            <div className="d-flex align-items-center">
-                                                <p className="m-0">{image.id}</p>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-
-                    {!arrayDatas &&
-                        BeforeInit === true &&
-                        defaultValues.map((movie, index) => (
-                            <Col key={`key-default-${index}`} sm={12} md={6} lg={4}>
-                                <Card className="m-2 mt-5">
-                                    <Card.Img variant="top" src={movie.img} className="dimension-card-img" />
-                                    <Card.Body>
-                                        <Card.Title>{movie.title}</Card.Title>
-                                        <Card.Text>{movie.parag}</Card.Text>
-                                        <div className="d-flex justify-content-between">
-                                            <div>
-                                                {" "}
-                                                <Button variant="btn btn-outline-secondary">{movie.textbutt1}</Button>
-                                                <Button className="ms-2" variant="btn btn-outline-secondary">
-                                                    {movie.textbutt2}
-                                                </Button>
-                                            </div>
-                                            <p>{movie.time}</p>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-                </div>
+                <SingleCard
+                    arrayDatas={arrayDatas}
+                    cardHidden={cardHidden}
+                    handleCardHidden={handleCardHidden}
+                    downloadImage={downloadImage}
+                    BeforeInit={BeforeInit}
+                    defaultValues={defaultValues}
+                />
             </Container>
         </div>
     );
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+
+const SingleCard = (props) => {
+    const { arrayDatas, cardHidden, handleCardHidden, downloadImage, BeforeInit, defaultValues } = props;
+
+    return (
+        <>
+            <div className="d-flex flex-wrap">
+                {arrayDatas &&
+                    arrayDatas.slice(0, 9).map((image, index) => (
+                        <Col
+                            style={{ display: cardHidden.includes(image.id) ? "none" : "block" }}
+                            key={`key-fetch-${index}`}
+                            sm={12}
+                            md={6}
+                            lg={4}
+                        >
+                            <Card className="m-2 mt-5 shadow">
+                                <Card.Img variant="top" src={image.src.original} className="dimension-card-img" />
+                                <Card.Body style={{ minHeight: "320px" }}>
+                                    <Card.Title>{image.alt}</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of the
+                                        card's content.
+                                    </Card.Text>
+                                    <Card.Text className="fw-bold">{image.photographer}</Card.Text>
+                                    <div className="d-flex flex-column">
+                                        {" "}
+                                        <a className="m-1" href={image.src.original}>
+                                            Guarda l'originale
+                                        </a>
+                                        <a className="m-1" href={image.photographer_url}>
+                                            Vai al profilo dell'autore
+                                        </a>
+                                    </div>
+
+                                    <div className="d-flex justify-content-between">
+                                        <div className="d-flex gap-2 mt-2">
+                                            {" "}
+                                            <Button variant="btn btn-outline-secondary">View</Button>
+                                            <Button
+                                                onClick={() => {
+                                                    handleCardHidden(image.id);
+                                                }}
+                                                variant="btn btn-outline-secondary"
+                                            >
+                                                Hide
+                                            </Button>
+                                            <Button
+                                                onClick={() => downloadImage(image.src.original, image.alt)}
+                                                variant="btn btn-outline-primary"
+                                            >
+                                                Download
+                                            </Button>
+                                        </div>
+                                        <div className="d-flex align-items-center">
+                                            <p className="m-0">{image.id}</p>
+                                        </div>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+
+                {!arrayDatas &&
+                    BeforeInit === true &&
+                    defaultValues.map((movie, index) => (
+                        <Col key={`key-default-${index}`} sm={12} md={6} lg={4}>
+                            <Card className="m-2 mt-5">
+                                <Card.Img variant="top" src={movie.img} className="dimension-card-img" />
+                                <Card.Body>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text>{movie.parag}</Card.Text>
+                                    <div className="d-flex justify-content-between">
+                                        <div>
+                                            {" "}
+                                            <Button variant="btn btn-outline-secondary">{movie.textbutt1}</Button>
+                                            <Button className="ms-2" variant="btn btn-outline-secondary">
+                                                {movie.textbutt2}
+                                            </Button>
+                                        </div>
+                                        <p>{movie.time}</p>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default SingleCard;
